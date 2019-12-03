0f2af82579a15a4495bb7486c6927e774e54d6c6
Pranbir <pranbirsarkar@gmail.com>
2019-12-03 20:15:48 +0530

    Dynamic pages added with basic contents and routing and def

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('Dashboard', views.header, name='Dashboard'),
     path('header', views.header, name='header'),
+    path('Transaction', views.Transaction, name='Transaction'),
+    path('Incomes', views.Incomes, name='Incomes'),
+    path('Expenses', views.Expenses, name='Expenses'),
+    path('Accounts', views.Accounts, name='Accounts'),
+    path('Budgets', views.Budgets, name='Budgets'),
+    path('Income_Categories', views.Income_Categories, name='Income_Categories'),
+    path('Expense_Categories', views.Expense_Categories, name='Expense_Categories'),
+    path('Income_Vs_Expense', views.Income_Vs_Expense, name='Income_Vs_Expense'),
+    path('Income_Calendar', views.Income_Calendar, name='Income_Calendar'),
+    path('Expense_Calendar', views.Expense_Calendar, name='Expense_Calendar'),
+    path('Manage_Profile', views.Manage_Profile, name='Manage_Profile'),
+    path('Logout', views.Logout, name='Logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,7 +4,62 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'base.html')
+    return render(request, 'main/header_footer.html')
+
+
+def Dashboard(request):
+    return render(request, 'main/Dashboard.html')
+
 
 def header(request):
     return render(request, 'main/header_footer.html')
+
+def Transaction(request):
+    return render(request, 'main/Transaction.html')
+
+
+def Incomes(request):
+    return render(request, 'main/Incomes.html')
+
+
+def Expenses(request):
+    return render(request, 'main/Expenses.html')
+
+
+def Accounts(request):
+    return render(request, 'main/Accounts.html')
+
+
+
+def Budgets(request):
+    return render(request, 'main/Budgets.html')
+
+
+
+def Income_Categories(request):
+    return render(request, 'main/Income_Categories.html')
+
+
+def Expense_Categories(request):
+    return render(request, 'main/Expense_Categories.html')
+
+
+def Income_Vs_Expense(request):
+    return render(request, 'main/Income_Vs_Expense.html')
+
+
+
+def Income_Calendar(request):
+    return render(request, 'main/Income_Calendar.html')
+
+
+def Expense_Calendar(request):
+    return render(request, 'main/Expense_Calendar.html')
+
+
+def Manage_Profile(request):
+    return render(request, 'main/Manage_Profile.html')
+
+
+def Logout(request):
+    return render(request, 'main/Logout.html')
